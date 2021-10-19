4c3c044eb47dfd94b43bca019eca6965ae23cf19
Johandro Gonzalez <88999789+JohandroG@users.noreply.github.com>
2021-10-18 18:11:54 -0600

    Add files via upload

---
+++ First Java Program/JavaProgram.java
@@ -0,0 +1,9 @@
+public class JavaProgram{
+
+    public static void main(String[] args) {
+        System.out.println("My name is Johandro Gonzalez");
+        System.out.println("I am 21 years old");
+        System.out.println("My hometown is Cuidad Neily");
+    }
+}
+
